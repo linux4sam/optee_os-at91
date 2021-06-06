c173f47ecdd13577f7d41577123a7b892c7e5c71
Clément Léger <clement.leger@bootlin.fr>
2021-06-06 17:30:46 +0200

    plat-sam: add SCMI server foundation
    Signed-off-by: Clément Léger <clement.leger@bootlin.com>

--- core/arch/arm/plat-sam/conf.mk
+++ core/arch/arm/plat-sam/conf.mk
@@ -44,6 +44,9 @@ CFG_MMAP_REGIONS ?= 24
 CFG_SHMEM_START  ?= 0x21000000
 CFG_SHMEM_SIZE   ?= 0x400000
 
+CFG_SCMI_SHMEM_START  ?= 0x21400000
+CFG_SCMI_SHMEM_SIZE   ?= 0x1000
+
 CFG_TEE_RAM_VA_SIZE ?= 0x100000
 
 # Device tree related configuration
@@ -92,3 +95,13 @@ CFG_DRIVERS_PINCTRL ?= y
 CFG_ATMEL_PIO ?= y
 CFG_DRIVERS_GPIO ?= y
 endif
+
+# SCMI related configuration
+CFG_SCMI_PTA ?= y
+
+CFG_SCMI_MSG_DRIVERS ?= y
+ifeq ($(CFG_SCMI_MSG_DRIVERS),y)
+$(call force,CFG_SCMI_MSG_SMT,y)
+$(call force,CFG_SCMI_MSG_CLOCK,y)
+endif
+
--- core/arch/arm/plat-sam/nsec-service/smc_ids.h
+++ core/arch/arm/plat-sam/nsec-service/smc_ids.h
@@ -5,11 +5,16 @@
 
 #ifndef SMC_IDS_H
 #define SMC_IDS_H
+#include <optee_msg.h>
+#include <sm/optee_smc.h>
 
 #define SAMA5_SMC_SIP_SFR_SET_USB_SUSPEND	0x300
 
 #define SAMA5_SMC_SIP_SET_SUSPEND_MODE	0x400
 #define SAMA5_SMC_SIP_GET_SUSPEND_MODE	0x401
+
+/* SCMI */
+#define SAMA5_SMC_SIP_SCMI_CALL_ID	0x200
 
 /* SAMA5 SMC return codes */
 #define SAMA5_SMC_SIP_RETURN_SUCCESS	0x0
