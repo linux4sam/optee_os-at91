7d574da3d8d843b22b96d522c9ac4c540a64a55f
Clément Léger <clement.leger@bootlin.fr>
2021-06-07 19:00:16 +0200

    plat-sam: add sm_platform_handler
    Signed-off-by: Clément Léger <clement.leger@bootlin.com>

---
+++ core/arch/arm/plat-sam/nsec-service/smc_ids.h
@@ -0,0 +1,15 @@
+/* SPDX-License-Identifier: BSD-2-Clause */
+/*
+ * Copyright (c) 2021, Bootlin
+ */
+
+#ifndef SMC_IDS_H
+#define SMC_IDS_H
+
+/* L2CC  */
+#define SAMA5_SMC_SIP_L2X0_WRITE_REG	0x100
+
+/* SAMA5 SMC return codes */
+#define SAMA5_SMC_SIP_RETURN_SUCCESS	0x0
+
+#endif /* SMC_IDS_H */
