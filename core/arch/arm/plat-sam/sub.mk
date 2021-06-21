4bb78930fd2f13f1ece2a31a707e45e9773e6849
Clément Léger <clement.leger@bootlin.com>
2021-06-21 16:21:44 +0200

    plat-sam: add stub for PSCI
    In order to use SCMI from Linux, PsCI must be enable to probe SMCCC version.
    This stub only support ARM_SMCCC_VERSION feature to allow probing it correctly.
    
    Signed-off-by: Clément Léger <clement.leger@bootlin.com>

--- core/arch/arm/plat-sam/sub.mk
+++ core/arch/arm/plat-sam/sub.mk
@@ -4,5 +4,5 @@ srcs-$(CFG_AT91_MATRIX) += matrix.c
 srcs-$(CFG_PL310) += sam_pl310.c
 srcs-$(CFG_SCMI_MSG_DRIVERS) += scmi_server.c
 
-subdirs-y += nsec-service
+subdirs-y += nsec-service pm
 subdirs-y += drivers
