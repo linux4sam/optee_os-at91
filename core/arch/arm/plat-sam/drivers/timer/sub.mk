480051a399319250cc584c033c63a2fbce0bc28a
Clément Léger <clement.leger@bootlin.com>
2021-06-23 15:55:39 +0200

    plat-sam: add timer clock enable driver
    This patch allows to enable the timer clocks when using them with Linux.
    Indeed, the tcb timers are used as clocksource in Linux. However, if the
    clocks are handled using SCMI, they are not probed early enough for the
    clocksource to be setup. In order to workaround that, fixed-clock are
    used but the real hardware clock must be enabled by OP-TEE before.
    This code is meant to do that.
    
    Signed-off-by: Clément Léger <clement.leger@bootlin.com>

---
+++ core/arch/arm/plat-sam/drivers/timer/sub.mk
@@ -0,0 +1,2 @@
+# This driver depends on the timers being available
+srcs-$(CFG_ATMEL_TCB) += atmel_tcb.c
--- core/arch/arm/plat-sam/sub.mk
+++ core/arch/arm/plat-sam/sub.mk
@@ -5,4 +5,5 @@ srcs-$(CFG_PL310) += sam_pl310.c
 srcs-$(CFG_SCMI_MSG_DRIVERS) += scmi_server.c
 
 subdirs-y += pm
+subdirs-y += drivers
 subdirs-y += nsec-service
