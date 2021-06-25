81a7a607ac8b8f38cf3a1936cedf9df935997127
Clément Léger <clement.leger@bootlin.com>
2021-06-25 14:19:26 +0200

    drivers: clk: add devicetree support
    When using a devicetree, it is often useful to have clocks parsing.
    This support adds clocks properties parsing and allow having clock
    providers and users. Clocks drivers can also be declared with
    CLK_OF_DECLARE. They will be probed automatically by the clock core.
    On the user side, function clk_of_get_by_name and clk_of_get_by_idx
    allows to retrieve a clock from the device tree description and match
    it with the provider clocks. The core ensure the clocks are probed
    hierarchically.
    
    Signed-off-by: Clément Léger <clement.leger@bootlin.com>

--- core/drivers/clk/clk.c
+++ core/drivers/clk/clk.c
@@ -353,3 +353,263 @@ int clk_scmi_get_rates_array(struct clk *clk, size_t start_index,
 	return SCMI_SUCCESS;
 }
 #endif
+
+#if defined(CFG_DT)
+
+struct clk *clk_of_get_simple_clk(struct clk_of_phandle_args *args __unused,
+				  void *data)
+{
+	return data;
+}
+
+struct clk_of_provider {
+	int nodeoffset;
+	struct clk *(*get_of_clk)(struct clk_of_phandle_args *args, void *data);
+	void *data;
+	SLIST_ENTRY(clk_of_provider) link;
+};
+
+static SLIST_HEAD(, clk_of_provider) clk_of_provider_list =
+				SLIST_HEAD_INITIALIZER(clk_of_provider_list);
+
+int clk_of_register_clk_provider(int nodeoffset,
+				 struct clk *(get_of_clk)(struct clk_of_phandle_args *, void *),
+				 void *data)
+{
+	struct clk_of_provider *prv;
+
+	prv = malloc(sizeof(*prv));
+	if (!prv)
+		return -1;
+
+	prv->get_of_clk = get_of_clk;
+	prv->data = data;
+	prv->nodeoffset = nodeoffset;
+
+	SLIST_INSERT_HEAD(&clk_of_provider_list, prv, link);
+
+	return 0;
+}
+
+static struct clk_of_provider *clk_get_provider(int nodeoffset)
+{
+	struct clk_of_provider *prv;
+
+	SLIST_FOREACH(prv, &clk_of_provider_list, link) {
+		if (prv->nodeoffset == nodeoffset)
+			return prv;
+	}
+
+	return NULL;
+}
+
+static uint32_t fdt_clock_cells(void *fdt, int nodeoffset)
+{
+	const fdt32_t *c;
+	int len;
+
+	c = fdt_getprop(fdt, nodeoffset, "#clock-cells", &len);
+	if (!c)
+		return len;
+
+	if (len != sizeof(*c))
+		return -FDT_ERR_BADNCELLS;
+
+	return fdt32_to_cpu(*c);
+}
+
+int clk_of_get_idx_by_name(void *fdt, int nodeoffset, const char *name)
+{
+	int idx;
+
+	idx = fdt_stringlist_search(fdt, nodeoffset, "clock-names", name);
+	if (idx < 0)
+		return -1;
+
+	return idx;
+}
+
+static struct clk *clk_of_get_from_provider(int nodeoffset,
+					    unsigned int clock_cells,
+					    const uint32_t *prop)
+{
+	unsigned int arg;
+	struct clk_of_phandle_args pargs;
+	struct clk_of_provider *prv;
+
+	pargs.args_count = clock_cells;
+	for (arg = 0; arg < clock_cells; arg++)
+		pargs.args[arg] = fdt32_to_cpu(prop[arg + 1]);
+
+	prv = clk_get_provider(nodeoffset);
+	if (!prv)
+		return NULL;
+
+	return prv->get_of_clk(&pargs, prv->data);
+}
+
+struct clk *clk_of_get_by_name(void *fdt, int nodeoffset,
+			       const char *name)
+{
+	int clk_id;
+
+	clk_id = clk_of_get_idx_by_name(fdt, nodeoffset, name);
+	if (clk_id < 0)
+		return NULL;
+
+	return clk_of_get_by_idx(fdt, nodeoffset, clk_id);
+}
+
+struct clk *clk_of_get_by_idx(void *fdt, int nodeoffset, int clk_idx)
+{
+	uint32_t phandle;
+	const uint32_t *prop;
+	int len, idx = 0, parent_node, clock_cells, idx32;
+
+	prop = fdt_getprop(fdt, nodeoffset, "clocks", &len);
+	if (!prop)
+		return NULL;
+
+	while (idx < len) {
+		idx32 = idx / sizeof(uint32_t);
+		phandle = fdt32_to_cpu(prop[idx32]);
+
+		parent_node = fdt_node_offset_by_phandle(fdt, phandle);
+		if (parent_node < 0)
+			return NULL;
+
+		clock_cells = fdt_clock_cells(fdt, parent_node);
+		if (clk_idx == 0)
+			return clk_of_get_from_provider(parent_node,
+							clock_cells,
+							&prop[idx32]);
+
+		clk_idx--;
+		idx += sizeof(phandle) + clock_cells * sizeof(uint32_t);
+	}
+
+	return NULL;
+}
+
+static const struct clk_driver *clk_get_compatible_driver(const char *compat)
+{
+	const struct dt_device_match *dm;
+	const struct dt_driver *drv;
+	const struct clk_driver *clk_drv;
+
+	for_each_dt_driver(drv) {
+		if (drv->type != DT_DRIVER_CLK)
+			continue;
+
+		clk_drv = (const struct clk_driver *)drv->driver;
+		for (dm = drv->match_table; dm; dm++) {
+			if (!dm->compatible)
+				break;
+			if (strcmp(dm->compatible, compat) == 0)
+				return clk_drv;
+		}
+	}
+
+	return NULL;
+}
+
+static void clk_setup_compatible(void *fdt, const char *compatible,
+				 const struct clk_driver *clk_drv);
+
+static void probe_parent_clock(void *fdt, int nodeoffset)
+{
+	const char *compat;
+	int idx, count, len;
+	const struct clk_driver *clk_drv;
+
+	count = fdt_stringlist_count(fdt, nodeoffset, "compatible");
+	if (count < 0)
+		return;
+
+	for (idx = 0; idx < count; idx++) {
+		compat = fdt_stringlist_get(fdt, nodeoffset, "compatible", idx,
+					    &len);
+		if (!compat)
+			return;
+
+		clk_drv = clk_get_compatible_driver(compat);
+		if (clk_drv)
+			clk_setup_compatible(fdt, compat, clk_drv);
+	}
+}
+
+static void parse_clock_property(void *fdt, int nodeoffset)
+{
+	uint32_t phandle;
+	const uint32_t *prop;
+	int len, idx = 0, parent_node, clock_cells;
+
+	prop = fdt_getprop(fdt, nodeoffset, "clocks", &len);
+	if (!prop)
+		return;
+
+	while (idx < len) {
+		phandle = fdt32_to_cpu(prop[idx]);
+
+		parent_node = fdt_node_offset_by_phandle(fdt, phandle);
+		if (parent_node < 0)
+			return;
+
+		probe_parent_clock(fdt, parent_node);
+
+		clock_cells = fdt_clock_cells(fdt, parent_node);
+		idx += sizeof(phandle) + clock_cells * sizeof(uint32_t);
+	}
+}
+
+static void clk_setup_compatible(void *fdt, const char *compatible,
+				 const struct clk_driver *clk_drv)
+{
+	int status;
+	int node = fdt_node_offset_by_compatible(fdt, -1, compatible);
+
+	if (node < 0 || clk_get_provider(node))
+		return;
+
+	while (node >= 0) {
+		status = _fdt_get_status(fdt, node);
+		if (!(status & DT_STATUS_OK_SEC))
+			continue;
+
+		parse_clock_property(fdt, node);
+
+		clk_drv->setup(fdt, node);
+
+		node = fdt_node_offset_by_compatible(fdt, node, compatible);
+	};
+}
+
+static TEE_Result clk_probe(void)
+{
+	const struct dt_device_match *dm;
+	const struct dt_driver *drv;
+	void *fdt = get_embedded_dt();
+	const struct clk_driver *clk_drv;
+
+	IMSG("Probing clocks from device tree\n");
+	if (!fdt)
+		panic();
+
+	for_each_dt_driver(drv) {
+		if (drv->type != DT_DRIVER_CLK)
+			continue;
+
+		clk_drv = (const struct clk_driver *)drv->driver;
+		for (dm = drv->match_table; dm; dm++) {
+			if (!dm->compatible)
+				break;
+
+			clk_setup_compatible(fdt, dm->compatible, clk_drv);
+		}
+	}
+
+	return TEE_SUCCESS;
+}
+service_init(clk_probe);
+
+#endif
