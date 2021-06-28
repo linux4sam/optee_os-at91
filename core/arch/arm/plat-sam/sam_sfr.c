fb5aff0d9cec1842dc6d66e9af9b586539fc607d
Clément Léger <clement.leger@bootlin.com>
2021-06-28 17:50:09 +0200

    plat-sam: set SFR as secure if specified in devicetree
    Move SFR specific code to sam_sfr.c and if the SFR is set as secure
    only in the devicetree, set the SFR world as secure in the matrix.
    This way, the normal worlkd won't be able to access it.
    
    Signed-off-by: Clément Léger <clement.leger@bootlin.com>

---
+++ core/arch/arm/plat-sam/sam_sfr.c
@@ -0,0 +1,81 @@
+// SPDX-License-Identifier: BSD-2-Clause
+/*
+ * Copyright (C) 2017 Timesys Corporation.
+ * Copyright (C) 2021 Bootlin
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <io.h>
+#include <kernel/boot.h>
+#include <kernel/dt.h>
+#include <kernel/tz_ssvce_pl310.h>
+#include <libfdt.h>
+#include <matrix.h>
+#include <mm/core_mmu.h>
+#include <mm/core_memprot.h>
+#include <sm/sm.h>
+#include <smc_ids.h>
+#include <types_ext.h>
+#include <sama5d2.h>
+
+#include <sam_pl310.h>
+#include <sam_sfr.h>
+
+register_phys_mem_pgdir(MEM_AREA_IO_SEC, SFR_BASE, CORE_MMU_PGDIR_SIZE);
+
+vaddr_t sam_sfr_base(void)
+{
+	static void *va;
+
+	if (cpu_mmu_enabled()) {
+		if (!va)
+			va = phys_to_virt(SFR_BASE, MEM_AREA_IO_SEC, 1);
+		return (vaddr_t)va;
+	}
+	return SFR_BASE;
+}
+
+static TEE_Result atmel_sfr_probe(const void *fdt, int node,
+				  const void *compat_data __unused)
+{
+	if (_fdt_get_status(fdt, node) != DT_STATUS_OK_SEC)
+		return TEE_ERROR_GENERIC;
+	
+	matrix_configure_periph_secure(AT91C_ID_SFR);
+
+	return TEE_SUCCESS;
+}
+
+static const struct dt_device_match atmel_sfr_match_table[] = {
+	{ .compatible = "atmel,sama5d2-sfr" },
+	{ }
+};
+
+DEFINE_DT_DRIVER(atmel_sfr_dt_driver) = {
+	.name = "atmel_sfr",
+	.type = DT_DRIVER_NOTYPE,
+	.match_table = atmel_sfr_match_table,
+	.probe = atmel_sfr_probe,
+};
