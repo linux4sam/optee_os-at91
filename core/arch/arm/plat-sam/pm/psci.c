f6ed01aacdf48c2679751bd7a384ab7e73efda6d
Clément Léger <clement.leger@bootlin.com>
2021-07-19 19:52:47 +0200

    plat-sam: enable PSCI shutdown support
    Enable PSCI shutdown support using at91 shutdown controller driver.
    
    Signed-off-by: Clément Léger <clement.leger@bootlin.com>

--- core/arch/arm/plat-sam/pm/psci.c
+++ core/arch/arm/plat-sam/pm/psci.c
@@ -4,12 +4,24 @@
  */
 
 #include "at91_rstc.h"
+#include "at91_shdwc.h"
 #include <console.h>
 #include <kernel/panic.h>
 #include <sm/psci.h>
 #include <sm/std_smc.h>
 #include <stdint.h>
 #include <trace.h>
+
+void __noreturn psci_system_off(void)
+{
+	if (!at91_shdwc_available())
+		panic();
+
+	if (TRACE_LEVEL >= TRACE_DEBUG)
+		console_flush();
+
+	at91_shdwc_shutdown();
+}
 
 void __noreturn psci_system_reset(void)
 {
@@ -28,6 +40,10 @@ int psci_features(uint32_t psci_fid)
 		return PSCI_RET_SUCCESS;
 	case PSCI_SYSTEM_RESET:
 		if (at91_rstc_available())
+			return PSCI_RET_SUCCESS;
+		return PSCI_RET_NOT_SUPPORTED;
+	case PSCI_SYSTEM_OFF:
+		if (at91_shdwc_available())
 			return PSCI_RET_SUCCESS;
 		return PSCI_RET_NOT_SUPPORTED;
 	default:
