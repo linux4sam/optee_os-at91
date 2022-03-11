OP-TEE/optee_os

drivers: rtc: add RTC API
This API allows to interact with a RTC registered as the system RTC.

Reviewed-by: Etienne Carriere <etienne.carriere@linaro.org>
Acked-by: Jens Wiklander <jens.wiklander@linaro.org>
Signed-off-by: Clément Léger <clement.leger@bootlin.com>

core/drivers/rtc/sub.mk
@@ -1,0 +1,1 @@
+srcs-$(CFG_DRIVERS_RTC) += rtc.c

mk/config.mk
@@ -819,3 +819,6 @@ endif
 # extension. When set to 'n', the plat_get_freq() function must be defined by
 # the platform code
 CFG_CORE_HAS_GENERIC_TIMER ?= y
+
+# Enable RTC API
+CFG_DRIVERS_RTC ?= n

linux4sam/optee_os-at91

core/drivers/rtc/sub.mk
@@ -1,0 +1,1 @@
+srcs-$(CFG_RTC) += rtc.c

mk/config.mk
@@ -793,3 +793,6 @@ CFG_WDT_SM_HANDLER_ID ?= 0x82003D06
 $(eval $(call cfg-enable-all-depends,CFG_WDT_SM_HANDLER,CFG_WDT))
 
 CFG_CORE_HAS_GENERIC_TIMER ?= y
+
+# Enable RTC API
+CFG_RTC ?= n